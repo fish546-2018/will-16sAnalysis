203e8ce59bac8102a7fedf2eba6c6e00ab26d12e
Will Sano <wsano@uw.edu>
2018-11-09 16:10:33 -0800

    refined wsano-fastqc.sh + added analyses to .gitignore

--- scripting-exercise/wsano-fastqc.sh
+++ scripting-exercise/wsano-fastqc.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 sample_name=$(basename -s ".fq.gz" "$1")
 
-gunzip scripting_raw/${sample_name}.fq.gz -k | FastQC/fastqc scripting_raw/${sample_name}.fq
+gunzip scripting_raw/${sample_name}.fq.gz -k | FastQC/fastqc scripting_raw/${sample_name}.fq -o analyses
 
+rm analyses/${sample_name}_fastqc.zip
 echo "finished FastQC of" ${sample_name}
